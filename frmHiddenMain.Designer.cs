23d09331ede73a698ef03ff74dc59d4aa3e73786
Walid Amro <60905923+walidamro-msft@users.noreply.github.com>
2020-06-12 17:58:46 -0500

    Initial commit.

---
+++ frmHiddenMain.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Pinger
+{
+    partial class frmHiddenMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHiddenMain));
+            this.notifyIconStatus = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pingTimer = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pingNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIconStatus
+            // 
+            this.notifyIconStatus.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIconStatus.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconStatus.Icon")));
+            this.notifyIconStatus.Text = "Pinger (Initializing)";
+            this.notifyIconStatus.Visible = true;
+            // 
+            // pingTimer
+            // 
+            this.pingTimer.Tick += new System.EventHandler(this.pingTimer_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.pingNowToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(301, 206);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(300, 38);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(300, 38);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(300, 38);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // pingNowToolStripMenuItem
+            // 
+            this.pingNowToolStripMenuItem.Name = "pingNowToolStripMenuItem";
+            this.pingNowToolStripMenuItem.Size = new System.Drawing.Size(300, 38);
+            this.pingNowToolStripMenuItem.Text = "Ping Now";
+            this.pingNowToolStripMenuItem.Click += new System.EventHandler(this.pingNowToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(297, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconStatus;
+        private System.Windows.Forms.Timer pingTimer;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pingNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
+
---
+++ frmHiddenMain.cs
@@ -0,0 +1,136 @@
+﻿/*
+# This Sample Code is provided for the purpose of illustration only and is not intended to be used 
+# in a production environment. THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" 
+# WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. We grant You a nonexclusive, 
+# royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code 
+# form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to 
+# market Your software product in which the Sample Code is embedded; (ii) to include a valid copyright 
+# notice on Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold 
+# harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ 
+# fees, that arise or result from the use or distribution of the Sample Code.
+
+# This sample script is not supported under any Microsoft standard support program or service. 
+# The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims 
+# all implied warranties including, without limitation, any implied warranties of merchantability 
+# or of fitness for a particular purpose. The entire risk arising out of the use or performance of 
+# the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, 
+# or anyone else involved in the creation, production, or delivery of the scripts be liable for any 
+# damages whatsoever (including, without limitation, damages for loss of business profits, business 
+# interruption, loss of business information, or other pecuniary loss) arising out of the use of or 
+# inability to use the sample scripts or documentation, even if Microsoft has been advised of the 
+# possibility of such damages 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pinger
+{
+    public partial class frmHiddenMain : Form
+    {
+        public Networking.PingerStatuses PingerStatus = Networking.PingerStatuses.Initializing;
+        
+        public frmHiddenMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            pingTimer.Interval = int.Parse(ConfigurationManager.AppSettings["PingIntervalInSeconds"]) * 1000;
+            pingTimer.Enabled = true;
+            pingTimer.Start();
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = true;
+            pingNowToolStripMenuItem.Enabled = true;
+            AttemptToPing();
+
+
+        }
+
+        private void pingTimer_Tick(object sender, EventArgs e)
+        {
+            AttemptToPing();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AttemptToPing()
+        {
+            if (Networking.PingHost(ConfigurationManager.AppSettings["IpAddressOrHostnameToPing"]))
+            {
+                if (bool.Parse(ConfigurationManager.AppSettings["ShowNotificationWhenStatusChanges"]))
+                {
+                    if (PingerStatus != Networking.PingerStatuses.Alive)
+                    {
+                        notifyIconStatus.BalloonTipIcon = ToolTipIcon.Info;
+                        notifyIconStatus.BalloonTipTitle = $"{ConfigurationManager.AppSettings["IpAddressOrHostnameToPing"]} is alive...";
+                        notifyIconStatus.BalloonTipText = "Ping was successful...";
+                        notifyIconStatus.ShowBalloonTip(int.Parse(ConfigurationManager.AppSettings["NotificationTimeoutInSeconds"]) * 1000);
+
+                    }
+                }
+                notifyIconStatus.Icon = Properties.Resources.ConnectToRemoteServer;
+                notifyIconStatus.Text = $"Pinger (Alive) | {DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt")}";
+                PingerStatus = Networking.PingerStatuses.Alive;
+            }
+            else
+            {
+                if (bool.Parse(ConfigurationManager.AppSettings["ShowNotificationWhenStatusChanges"]))
+                {
+                    if (PingerStatus == Networking.PingerStatuses.Alive)
+                    {
+                        notifyIconStatus.BalloonTipIcon = ToolTipIcon.Warning;
+                        notifyIconStatus.BalloonTipTitle = $"{ConfigurationManager.AppSettings["IpAddressOrHostnameToPing"]} is unreachable!";
+                        notifyIconStatus.BalloonTipText = ConfigurationManager.AppSettings["NotificationFailureMessage"];
+                        notifyIconStatus.ShowBalloonTip(int.Parse(ConfigurationManager.AppSettings["NotificationTimeoutInSeconds"]) * 1000);
+
+                    }
+                }
+                notifyIconStatus.Icon = Properties.Resources.Disconnected;
+                notifyIconStatus.Text = $"Pinger (Unreachable) | {DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt")}";
+                PingerStatus = Networking.PingerStatuses.Unreachable;
+            }
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pingTimer.Start();
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = true;
+            pingNowToolStripMenuItem.Enabled = true;
+            notifyIconStatus.Text = "Pinger | [Started]";
+            AttemptToPing();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pingTimer.Stop();
+            startToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = false;
+            pingNowToolStripMenuItem.Enabled = false;
+            notifyIconStatus.Icon = Properties.Resources.Disconnected;
+            notifyIconStatus.Text = "Pinger | [Stopped]";
+            PingerStatus = Networking.PingerStatuses.Stopped;
+        }
+
+        private void pingNowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AttemptToPing();
+        }
+    }
+}
